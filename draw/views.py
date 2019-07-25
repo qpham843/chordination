67e0caff5a92fe618cc7c71575d69b873e119f5f
qpham843 <qpham843@berkeley.edu>
2019-07-25 03:28:29 -0400

    Created and did most of the interactive prototype.

--- draw/views.py
+++ draw/views.py
@@ -5,10 +5,6 @@ import json
 def index(request):
     return render(request, 'draw/index.html', {})
 
-def room(request, room_name):
-    return render(request, 'draw/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name))
-    })
+def temp(request):
+	return render(request, "draw/temp.html")
   
-def exerciseindex(request):
-    return render(request, 'draw/exerciseindex.html')
